7660824590ca4075455cda3fe2f5ba02c3c5cff7
Musab <musabpirzada1@gmail.com>
2023-08-08 11:21:36 +0500

    practice problem 2 sum together

---
+++ practice_problem_2/sum_together.js
@@ -0,0 +1,11 @@
+function sumTogether(arr1, arr2) {
+    const result = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        result.push(arr1[i] + arr2[i]);
+    }
+
+    return result;
+}
+
+module.exports = sumTogether;
