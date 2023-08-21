c6ef9d793b5f903ba46f7673809d28e448f70c15
Musab <musabpirzada1@gmail.com>
2023-08-21 07:13:09 +0500

    Sort by month update

--- Array Sort/sort_by_month.js
+++ Array Sort/sort_by_month.js
@@ -1,10 +1,15 @@
-function sortEventsByMonth(events) {
-  const monthOrder = {
-    'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6,
-    'JUL': 7, 'AUG': 8, 'SEP': 9, 'OCT': 10, 'NOV': 11, 'DEC': 12
-  };
+const MONTHS = [
+    'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+    'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+];
 
-  events.sort((a, b) => monthOrder[a.month] - monthOrder[b.month]);
+function sortEventsByMonth(events) {
+    const monthOrder = {
+        'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6,
+        'JUL': 7, 'AUG': 8, 'SEP': 9, 'OCT': 10, 'NOV': 11, 'DEC': 12
+    };
+
+    events.sort((a, b) => monthOrder[a.month] - monthOrder[b.month]);
 }
 
 module.exports = sortEventsByMonth;
