3479211d8fcd9e9de709f0c49781a40f82ba8a46
Musab <musabpirzada1@gmail.com>
2023-09-12 07:24:09 +0500

    Request order (Promises)

---
+++ promises/request_order.js
@@ -0,0 +1,21 @@
+const { makeFood } = require('./Kitchen');
+
+class Order {
+    constructor() {
+        this.isReady = false;
+    }
+
+    request(food) {
+        // Call makeFood and set up a callback function using .then
+        makeFood(food)
+            .then(() => {
+                // When the promise is resolved, this callback is executed.
+                this.isReady = true; // Set isReady to true since the food is ready.
+            })
+            .catch((error) => {
+                console.error('An error occurred while making the food:', error);
+            });
+    }
+}
+
+module.exports = Order;
